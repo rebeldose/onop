284cc9408c34647175837f8b768252f66c689e21
rebeldose <www.sartaj@gmail.com>
2020-08-15 23:21:21 +0530

    hamba hoii hoii hoii...

--- ra.R
+++ ra.R
@@ -1,6 +1,25 @@
+urls1 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx"
+download.file(urls1,destfile = "./Dataset/aquarium.xlsx")
+
+library(openxlsx)
+col <- 7:15
+row <- 18:23
+dat <- read.xlsx("./Dataset/aquarium.xlsx", sheet=1, 
+                 cols = col, rows = row, colNames =TRUE ,rowNames = TRUE)
+dat
 
 
-fileUrl1 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
-download.file(fileUrl1, destfile = "./Quiz1-01.csv", method = "curl")
-quiz1Data <- read.csv("Quiz1-01.csv")
-head(quiz1Data)
+library(XML)
+fileUrl3 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
+BalResto <- xmlTreeParse(sub("s", "", fileUrl3), useInternal=TRUE)
+rootNode <- xmlRoot(BalResto)
+
+zip <- xpathSApply(rootNode, "//zipcode", xmlValue)
+sum(zip == 21231)
+
+fileUrl4 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
+download.file(fileUrl4, destfile = "./Dataset/Quiz1-05.csv", method = "curl")
+
+library(data.table)
+DT <- fread("./Dataset/Quiz1-05.csv")
+DT
