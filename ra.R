f613f54e24625c1462a112b5c6b874fbef274469
rebeldose <www.sartaj@gmail.com>
2020-08-15 21:42:02 +0530

    excel file

--- ra.R
+++ ra.R
@@ -2,5 +2,5 @@
 
 fileUrl1 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
 download.file(fileUrl1, destfile = "./Quiz1-01.csv", method = "curl")
-quiz1Data <- read.csv("./Dataset/Quiz1-01.csv")
+quiz1Data <- read.csv("Quiz1-01.csv")
 head(quiz1Data)
