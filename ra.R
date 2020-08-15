47f122f1f788d59f3eac39a34cfd4d7a11471e0c
rebeldose <www.sartaj@gmail.com>
2020-08-15 20:30:08 +0530

    file downloaded

---
+++ ra.R
@@ -0,0 +1,6 @@
+
+
+fileUrl1 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
+download.file(fileUrl1, destfile = "./Quiz1-01.csv", method = "curl")
+quiz1Data <- read.csv("./Dataset/Quiz1-01.csv")
+head(quiz1Data)
